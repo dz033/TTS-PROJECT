57f0796aa1b574b3233cb28ece019d96664eef00
dzhao723 <121326266+dz033@users.noreply.github.com>
2023-08-08 10:48:29 +0800

    added soudns

--- tts.py
+++ tts.py
@@ -2,32 +2,48 @@
 #phonetics might be tricky 
 import winsound
 import requests
+import asyncio
 
 phoneticDict = {}
+phoneticListC = ["b", "d", "f", "g", "h", "dʒ", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "z" , "ʒ"
+                , "tʃ", "ʃ", "θ", "ð", "ŋ", "j"]
+phoneticListV = ["æ", "eɪ", "ɛ", "i:", "ɪ", "aɪ", "ɒ", "oʊ", "ʊ", "ʌ", "u:", "ɔɪ", "aʊ", "ə", "eəʳ", "ɑ:"
+                 , "ɜ:ʳ", "ɔ:", "ɪəʳ", "ʊəʳ"]
+phoneticListAll = phoneticListC + phoneticListV #not sure if they would be more useful together or separated
 
 def getPhonetics(word):
     #grab the phonetics string from dictionary api
     response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
     info = response.json()
     phonetics = info[0]["phonetic"]
-    print(phonetics)
-
+    return phonetics.strip("/")
 
 def getFox(): #for me to understand how to use api requests :)
     response = requests.get("https://randomfox.ca/floof/")
     print(response.text)
 
+def pronounceWord(phonetics):
+    for char in phonetics:
+        if char in phoneticListAll:
+            pronounce(char)
+        elif char == "ˈ":
+            pass
+
+
+
 def pronounce(phoneme):
-    winsound.PlaySound("poopootesting.wav", winsound.SND_FILENAME)
+    winsound.PlaySound(f"{phoneme}.wav", winsound.SND_FILENAME)
     return None
 
 def main():
 
     sampleText = input("Type or paste a paragraph. (only text at the moment please!)")
     l1 = sampleText.split(" ")
-    #for word in l1:
-    #    pronounce(phoneticDict[word])
+    for word in l1:
+        pronounceWord(word)
+        #add some kind of delay between words.
 
-#pronounce("a")
+#pronounce("poopootesting")
 #getFox()
-getPhonetics("bet")
+#getPhonetics("bet")
+pronounceWord("fd") # pacing is not consistent... hm
